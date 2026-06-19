99741ba02acc6e0dff92d9f3ee2e2d078327f24a
Emmanuele <emmanuele.surdi@gmail.com>
2026-06-19 21:53:33 +0200

    Fix Prisma client per Vercel

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,7 +1,14 @@
 import { PrismaPg } from "@prisma/adapter-pg";
 import { PrismaClient } from "@prisma/client";
 
+const globalForPrisma = globalThis as unknown as {
+  prisma: PrismaClient | undefined;
+};
+
 const adapter = new PrismaPg({ connectionString: process.env.DATABASE_URL! });
-const prisma = new PrismaClient({ adapter });
+
+export const prisma = globalForPrisma.prisma ?? new PrismaClient({ adapter });
+
+if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
 
 export default prisma;
