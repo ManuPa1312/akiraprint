91471c6307329cdeba143a8b8794d5e1edf72013
Emmanuele <emmanuele.surdi@gmail.com>
2026-06-27 12:12:14 +0200

    aggiungo log su middleware per vedere errore login

--- middleware.ts
+++ middleware.ts
@@ -6,11 +6,15 @@ export async function middleware(req: NextRequest) {
   const isAdminRoute = req.nextUrl.pathname.startsWith("/admin");
 
   if (isAdminRoute) {
+    console.log("Token:", JSON.stringify(token));
+    console.log("AUTH_SECRET presente:", !!process.env.AUTH_SECRET);
+    
     if (!token) {
       return NextResponse.redirect(new URL("/login", req.url));
     }
 
     if (token.role !== "admin") {
+      console.log("Role trovato:", token.role);
       return NextResponse.redirect(new URL("/", req.url));
     }
   }
